47a42112c0f04fa7e21ef44a4a29733631417657
Casper <draakie@proton.me>
2024-12-12 16:23:34 +0100

    wt: Add basic configuration.

---
+++ deploy/modules/windows-terminal.ps1
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+# Deploy windows terminal
+#-------------------------------------------------------------------------------
+
+# Script directory
+$base_dir = Resolve-Path -Path "$PSScriptRoot\..\.."
+
+# Imports
+. "$base_dir\utils\fs.ps1"
+
+# Deploy
+force_copy `
+    -source "$base_dir\config\windows-terminal\settings.json" `
+    -destination "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
