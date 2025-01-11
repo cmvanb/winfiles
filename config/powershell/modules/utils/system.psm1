527bae214551d15ac5a69db4c4b1cb75c140ed4b
Casper <draakie@proton.me>
2025-01-11 01:08:37 +0100

    deploy/scripts: Improve scripts deployment.

--- config/powershell/modules/utils/system.psm1
+++ config/powershell/modules/utils/system.psm1
@@ -14,4 +14,16 @@ function is_developer() {
     return (Get-WindowsDeveloperLicense -ErrorAction SilentlyContinue).IsValid
 }
 
+function set_registry_key($key, $name, $value) {
+    print " └> Set registry key: ``$key\$name`` -> ``$value``"
+
+    Set-ItemProperty -Path $key -Name $name -Value $value
+}
+
+function unset_registry_key($key, $name) {
+    print " └> Unset registry key: ``$key\$name``"
+
+    Remove-ItemProperty -Path $key -Name $name
+}
+
 Export-ModuleMember -Function *
--- deploy/modules/scripts.psm1
+++ deploy/modules/scripts.psm1
@@ -12,14 +12,26 @@ function deploy {
     print "└> Installing scripts."
 
     link_or_copy `
-        "$base_dir\scripts\print-environment.py" `
+        "$base_dir\config\scripts\print-environment.py" `
         "$env:USERPROFILE\.local\scripts\print-environment.py"
+
+    link_or_copy `
+        "$base_dir\config\scripts\prevent-screen-lock.ahk" `
+        "$env:USERPROFILE\.local\scripts\prevent-screen-lock.ahk"
+    set_registry_key `
+        -key "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" `
+        -name "PreventScreenLock" `
+        -value "$env:USERPROFILE\.local\scripts\prevent-screen-lock.ahk"
 }
 
 function undeploy {
     print "└> Uninstalling scripts."
 
     force_delete "$env:USERPROFILE\.local\scripts\print-environment.py"
+    force_delete "$env:USERPROFILE\.local\scripts\prevent-screen-lock.ahk"
+    unset_registry_key `
+        -key "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" `
+        -name "PreventScreenLock"
 }
 
 Export-ModuleMember -Function *
