e232eb4250c7802eb796ec5dabf98236d18a133a
Casper <casper@codemu.ch>
2026-02-21 23:25:49 +0100

    nvim: Replace duplicated config with submodule.

--- config/nvim/lua/utils/sleep.lua
+++
@@ -1,9 +0,0 @@
---------------------------------------------------------------------------------
--- Sleep
---------------------------------------------------------------------------------
-
-function _G.sleep(seconds)
-    local wake_time = tonumber(os.clock() + seconds);
-    while (os.clock() < wake_time) do
-    end
-end
