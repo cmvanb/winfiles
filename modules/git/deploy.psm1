2c8cccf01b7e9d8fc9e7f6c85ff2d5c170eab71b
Casper <draakie@proton.me>
2026-06-10 01:16:55 +0200

    git: Add global ignore file.

--- modules/git/deploy.psm1
+++ modules/git/deploy.psm1
@@ -14,12 +14,17 @@ function install {
     link_or_copy `
         -source "$base_dir\modules\git\src\.gitconfig" `
         -destination "$env:USERPROFILE\.gitconfig"
+
+    link_or_copy `
+        -source "$base_dir\modules\git\src\ignore" `
+        -destination "$env:USERPROFILE\.config\git\ignore"
 }
 
 function uninstall {
     Write-Host "└> Uninstalling git configuration."
 
     force_delete "$env:USERPROFILE\.gitconfig"
+    force_delete "$env:USERPROFILE\.config\git\ignore"
 }
 
 Export-ModuleMember -Function install, uninstall
